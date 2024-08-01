d857e4802cba6fb814df7a457354ad46301ae903
Mwan3kii <agathawanja303@gmail.com>
2024-08-01 22:30:03 +0300

    Fix line length

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -5,10 +5,13 @@ from typing import Mapping, Any, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Union[T, None] = None) -> Union[T, None]:
+def safely_get_value(
+    dct: Mapping[Any, T],
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[T, None]:
     """Return values add type annotations"""
     if key in dct:
         return dct[key]
     else:
         return default
-    
