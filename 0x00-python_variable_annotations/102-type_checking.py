ec38b3c2c513c7f665787714deeb08da86d1d0cd
Mwan3kii <agathawanja303@gmail.com>
2024-08-01 22:19:22 +0300

    Type checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Type checking"""
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> List[Any]:
+    """Changed to tuple to match the function type annotation"""
+    zoomed_in: List[Any] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
