f73b98b8b424677390304194f9fefca7844a0385
Mwan3kii <agathawanja303@gmail.com>
2024-08-06 21:24:33 +0300

    fix output error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """Coroutine called async generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator()() -> Generator[float, None, None]:
     """yield random number"""
     for _ in range(10):
         await asyncio.sleep(1)
