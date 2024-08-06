7d8b4856b6338979bc834491ad0bc2802707b116
Mwan3kii <agathawanja303@gmail.com>
2024-08-06 21:30:20 +0300

    fix output error

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ async_comprehension = __import__('1_async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """coroutine that will execute async_comprehension four times parallell"""
-    start = time.perf_counter()
+    start = time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end = time.perf_counter()
+    end = time()
     return end - start
