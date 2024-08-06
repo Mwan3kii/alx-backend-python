792cc8fbff2dd8648f21ad93b025dea0408a486a
Mwan3kii <agathawanja303@gmail.com>
2024-08-06 21:27:24 +0300

    fix output error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Import async_generator from previous task"""
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """takes function and returns random number"""
     return [i async for i in async_generator()]
