a6d63277da492a0ee7967f0b4179b095a910362d
Mwan3kii <agathawanja303@gmail.com>
2024-08-06 20:33:49 +0300

    fix output error

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,4 +8,4 @@ async def async_generator():
     """yield random number"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
