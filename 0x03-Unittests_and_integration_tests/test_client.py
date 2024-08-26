1dc369796666b9dfdb68585b58186a3365039734
Mwan3kii <agathawanja303@gmail.com>
2024-08-26 21:24:40 +0300

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock, Mock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -18,9 +18,12 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, {"payload": True})
 
-    @patch('client.GithubOrgClient.org', new_callable=property)
-    def test_public_repos_url(self, mock_org):
-        mock_org.return_value = {"repos_url": "https://api.github.com/orgs/google/repos"}
-        client = GithubOrgClient("google")
-        result = client._public_repos_url
-        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+    def test_public_repos_url(self):
+        """More patch"""
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
