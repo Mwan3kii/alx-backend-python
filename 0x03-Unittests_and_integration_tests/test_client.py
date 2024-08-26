f55df9ec4253cfeec02ac981882c005f720c7b37
Mwan3kii <agathawanja303@gmail.com>
2024-08-26 22:01:07 +0300

    Try new method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -12,16 +12,15 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",),
     ])
-    @patch('client.get_json', return_value={"payload": True})
-    def test_org(self, org_name, mock_get_json):
-        client = GithubOrgClient(org_name)
-        result = client.org
-        m_url = f"https://api.github.com/orgs/{org_name}"
-        mock_get_json.assert_called_once_with(m_url)
-        self.assertEqual(result, {"payload": True})
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
 
     def test_public_repos_url(self):
-        """More patch"""
+        """Test the publc repo ur property"""
         with patch('client.GithubOrgClient.org',
                    new_callable=PropertyMock) as mock:
             payload = {"repos_url": "World"}
