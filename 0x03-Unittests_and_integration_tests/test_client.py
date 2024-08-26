c30c09fa101f5970744023d2b0b48fea234b066b
Mwan3kii <agathawanja303@gmail.com>
2024-08-26 22:31:33 +0300

    Mock prperty

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,6 +2,7 @@
 """Intiatalizes test guthub client class"""
 import unittest
 from unittest.mock import patch
+from unittest.mock import PropertyMock, Mock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -18,3 +19,15 @@ class TestGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient(input)
         test_class.org()
         mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
+
+    def test_public_repos_url(self):
+        """ Test that the result of _public_repos_url is the expected one
+        based on the mocked payload
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
