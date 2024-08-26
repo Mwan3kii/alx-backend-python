f80bb7c466a28754a352dbf0bad738b4d456e919
Mwan3kii <agathawanja303@gmail.com>
2024-08-26 22:26:51 +0300

    Try ouput

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,14 +11,12 @@ from fixtures import expected_repos, apache2_repos
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for GithubOrgClient"""
     @parameterized.expand([
-        ("google", {"login": "google"}),
-        ("abc", {"login": "abc"}),
+        ('google'),
+        ('abc')
     ])
-    @patch('client.get_json', return_value={"login": "google"})
-    def test_org(self, org_name, expected_response, mock_get_json):
-        """Test that GithubOrgClient.org returns the correct value."""
-        client = GithubOrgClient(org_name)
-        result = client.org
-        self.assertEqual(result, expected_response)
-        m_url = f"https://api.github.com/orgs/{org_name}"
-        mock_get_json.assert_called_once_with(m_url)
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """Test that GithubOrgClient.org returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f'https://api.github.com/orgs/{input}')
